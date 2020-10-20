e3a37d28d79f02b7cbefcc5730d3c775a80188f2
Paul Yi <yipaulc@gmail.com>
2020-10-20 15:56:42 -0600

    Create Model and ViewModel

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,32 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+   var viewModel: EmojiMemoryGame
+   
+   var body: some View {
       HStack {
-         ForEach(0..<4) { index in
-            CardView(isFaceUp: false)
+         ForEach(viewModel.cards) { card in
+            CardView(card: card)
          }
       }
          .padding()
          .foregroundColor(Color.orange)
          .font(Font.largeTitle)
-    }
+   }
 }
 
 
 struct CardView: View {
-   var isFaceUp: Bool
+   var card: MemoryGame<String>.Card
+   
    var body: some View {
       ZStack {
-         if isFaceUp {
+         if card.isFaceUp {
             RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
             RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-            Text("👻")
+            Text(card.content)
             RoundedRectangle(cornerRadius: 10.0).fill()
          } else {
             RoundedRectangle(cornerRadius: 10.0).fill()
          }
-         
       }
    }
 }
+
+struct ContentView_Previews: PreviewProvider {
+   static var previews: some View {
+      ContentView(viewModel: EmojiMemoryGame())
+   }
+}
+
